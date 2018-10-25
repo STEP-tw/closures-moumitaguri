9ff049d38e52bbd2e575bd045b6e36b90ad57e86
moumita das <44020214+moumitaguri@users.noreply.github.com>
2018-10-25 11:49:40 +0530

    makeCounterFromNTest function is defined.
    makeCounterFromNTest.js test is passed.

--- src/lib.js
+++ src/lib.js
@@ -3,7 +3,13 @@ const makeConstant = function(arg){
     return arg;
   }
 }
-const makeCounterFromN = undefined;
+
+const makeCounterFromN = function(count){
+  return function(){
+    return count++;
+  }
+}
+
 const makeCounterFromZero = undefined;
 const makeDeltaTracker = undefined;
 const makeFiboGenerator = undefined;
